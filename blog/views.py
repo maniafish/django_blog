55cce4471da5ba38b1ec607cc8ed5a6a23ca4666
mania_fish <mania_fish@163.com>
2016-08-25 11:06:18 +0800

    表单相关操作

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
+from .forms import PostForm
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+        return HttpResponseRedirect((reverse('post_detail', args=(post.pk,))))
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return HttpResponseRedirect((reverse('post_detail', args=(post.pk,))))
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_delete(request, pk):
+    if request.method == "POST":
+        if "yes" in request.POST:
+            post = get_object_or_404(Post, pk=pk)
+            post.delete()
+            return HttpResponseRedirect((reverse('post_list')))
+        else:
+            return HttpResponseRedirect((reverse('post_detail', args=(pk,))))
+    else:
+        return render(request, 'blog/post_delete.html')
